53f0902dbc687da001439bf9b47fe0d23f5f6498
msubkhankulov <m.subkhankulov@gmail.com>
2018-05-05 16:48:18 -0700

    Add postgresql connection

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
+const { Pool } = require('pg')
 const PORT = process.env.PORT || 5000
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+})
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -16,5 +21,16 @@ express()
       result += i + ' '
     }
     res.send(result)
+  })
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table')
+      res.render('pages/db', result)
+      client.release()
+    } catch (err) {
+      console.error(err)
+      res.send('Error: ' + err)
+    }
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
