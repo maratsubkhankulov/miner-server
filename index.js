a13b972aa53bb537fc3635ed16652461b1301eb2
msubkhankulov <m.subkhankulov@gmail.com>
2018-05-05 17:26:05 -0700

    Turn off ssl for pg pool

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { Pool } = require('pg')
 const PORT = process.env.PORT || 5000
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: true
+  ssl: false
 })
 
 express()
