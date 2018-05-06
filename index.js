e81a487cf2d6c340b4752da85ed467a7fea5a4dd
msubkhankulov <m.subkhankulov@gmail.com>
2018-05-06 16:09:48 -0700

    Refactor /work and /submit APIs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const sha256 = require('sha256')
+const assert = require('assert')
 const { Pool } = require('pg')
 const PORT = process.env.PORT || 5000
 
@@ -13,21 +14,20 @@ const pool = new Pool({
 const bodyParser = require('body-parser')
 
 const difficultyTarget = 5;
-const jobs = [
-  {
-    jobId: 1,
-    clientId: 1,
-    blockHeader: {
-      version: 2,
-      prevBlockhash: "00000000000008a3a41b85b8b29ad444def299fee21793cd8b9e567eab02cd81",
-      merkleRoot: "2b12fcf1b09288fcaff797d71e950e71ae42b91e8bdb2304758dfcffc2b620e3",
-      timestamp: 1305998791,
-      difficultyTarget: 17,
-      Nonce: 2504433986,
-      blockHash: "myhash"
-    }
-  }
-]
+const nonceRange = 100; // Each job iterates over 100 nonces
+const nonceMax = 8589934591; // Max value of nonce
+
+const currentPuzzle = {
+  version: 2,
+  prevBlockhash: "00000000000008a3a41b85b8b29ad444def299fee21793cd8b9e567eab02cd81",
+  merkleRoot: "2b12fcf1b09288fcaff797d71e950e71ae42b91e8bdb2304758dfcffc2b620e3",
+  timestamp: 1305998791,
+  difficultyTarget: 17,
+}
+
+const solutions = []
+
+const jobs = []
 
 const clients = [
   {
@@ -36,24 +36,42 @@ const clients = [
   }
 ]
 
-function incrementNonce(job) {
-  job.blockHeader.Nonce += 1
+// A job will compute hashcash for puzzle and nonce values in range nonceRange
+// or client dies
+function createJob(clientId, puzzle) {
+  const job = {
+    jobId: jobs.length,
+    clientId: clientId,
+    nonceRange: nonceRange,
+    blockHeader: {},
+  }
+  job.blockHeader = currentPuzzle
+  job.blockHeader.nonce = Math.floor(Math.random() * Math.floor(nonceMax))
+  return job
 }
 
 function generatePuzzle(clientId, prevBlockhash) {
   const puzzle = {
-    jobId: jobs.length,
-    clientId: clientId,
-    blockHeader: {
       version: 2,
       prevBlockhash: prevBlockhash,
       merkleRoot: sha256('concatenated transaction hashes'),
       timestamp: (new Date).getTime(),
       difficultyTarget: difficultyTarget,
-      Nonce: 0,
-    }
   }
   return puzzle;
+}
+
+// TODO unit test
+function verifySolution(puzzle, nonce) {
+  const data = puzzle.version + puzzle.prevBlockhash + puzzle.markleRoot + puzzle.timestamp + puzzle.difficultyTarget + nonce
+  const hash = sha256(sha256(data))
+  var leadingZeros = 0
+  for (i = 0; i < puzzle.difficultyTarget; i++) {
+    if (hash[i] === '0') {
+      leadingZeros += 1
+    }
+  }
+  return leadingZeros == puzzle.difficultyTarget
 }
 
 express()
@@ -83,18 +101,31 @@ express()
       res.send('Error: ' + err)
     }
   })
+  // Server will not track client states, simply deal out jobs for them to do
+  // for some time
   .get('/work', async (req, res) => {
-    var job = jobs[jobs.length - 1]
-    if ('blockHash' in job.blockHeader) {
-      job = generatePuzzle(1, job.blockHeader.blockHash)
-      jobs.push(job)
-    } else {
-      incrementNonce(job)
+    if (currentPuzzle == null) {
+      currentPuzzle = generatePuzzle()
     }
+    const job = createJob(req.body.clientId, currentPuzzle)
+    jobs.push(job)
     res.send(JSON.stringify(job))
   })
+  // Clients submit nonce solutions. If the nonce is a real solution to the currentPuzzle
+  // record the solution and clear current puzzle
   .post('/submit', (req, res) => {
-    res.send('submitted')
+    if (currentPuzzle == null) {
+      res.status(400).send({error: 'No current puzzle.'})
+      return
+    }
+    if (verifySolution(currentPuzzle, req.body.nonce)) {
+      currentPuzzle.nonce = req.body.nonce
+      solutions.push(currentPuzzle)
+      currentPuzzle = null
+      res.send({correctSolution: true})
+    } else {
+      res.send({correctSolution: false})
+    }
   })
   .post('/tokens', (req, res) => {
     const token = {
